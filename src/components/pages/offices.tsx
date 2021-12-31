c7aefe8c406f9bb8f5e00ec0a9d0b23df1561790
Dimas M <dimas.m@softwareseni.com>
2021-12-31 16:14:56 +0700

    update: set-up environment

--- src/components/pages/offices.tsx
+++ src/components/pages/offices.tsx
@@ -1,7 +1,28 @@
-import React, { FC, ReactElement } from 'react'
-import { Title } from '@reapit/elements'
+import React, { FC, useEffect } from 'react'
 
-const Offices: FC<{}> = (): ReactElement => {
+import { Title } from '@reapit/elements'
+import { useReapitConnect } from '@reapit/connect-session'
+
+import { useGetOfficesQuery } from '../../generated/graphql'
+import graphQLRequestClient from '../../platform-api/graphQLClient'
+import { reapitConnectBrowserSession } from '../../core/connect-session'
+import { BASE_HEADERS } from '../../constants/api'
+
+const Offices: FC = () => {
+  const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
+
+  useEffect(() => {
+    if (!connectSession) return
+    graphQLRequestClient.setHeaders({
+      ...BASE_HEADERS,
+      authorization: connectSession.idToken,
+      'reapit-connect-token': connectSession.accessToken,
+    })
+  }, [connectSession])
+
+  const { data } = useGetOfficesQuery(graphQLRequestClient)
+
+  console.log(data)
   return <Title>Hellsso</Title>
 }
 
